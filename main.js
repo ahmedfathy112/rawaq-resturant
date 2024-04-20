7041166d6e23c56d5513747874964163d8819105
Ahmed Fathi <98648216+ahmedfathy112@users.noreply.github.com>
2024-04-21 00:41:22 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function updateClock() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  var timeString = hours + ":" + minutes + ":" + seconds;
+
+  document.getElementById("clock").innerText = timeString;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+// Initial call to display the clock immediately
+updateClock();
+// start cards sec
+var currentCardIndex = 0;
+var cards = document.querySelectorAll(".card-swip");
+var prevButton = document.getElementById("prevButton");
+var nextButton = document.getElementById("nextButton");
+
+function showCard(index) {
+  cards.forEach((card) => {
+    card.style.display = "none";
+  });
+  cards[index].style.display = "flex";
+}
+
+showCard(currentCardIndex);
+
+prevButton.addEventListener("click", function () {
+  currentCardIndex = (currentCardIndex - 1 + cards.length) % cards.length;
+  showCard(currentCardIndex);
+});
+
+nextButton.addEventListener("click", function () {
+  currentCardIndex = (currentCardIndex + 1) % cards.length;
+  showCard(currentCardIndex);
+});
